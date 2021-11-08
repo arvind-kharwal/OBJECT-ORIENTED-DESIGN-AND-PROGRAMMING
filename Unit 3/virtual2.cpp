c12bd9778946abb76d9d478bea4a8d2fe6d4486c
Arvind Kharwal <karvind629@gmail.com>
2021-11-08 12:11:50 +0530

    8 November 2021

--- Unit 3/virtual2.cpp
+++ Unit 3/virtual2.cpp
@@ -3,12 +3,15 @@ using namespace std;
 class A
 {
     public:
-       virtual void display()
+        void display()
         {
             cout<<"Display function from class A"<<endl;
         }
+        virtual void show()
+        {
+            cout<<"Show function from class A"<<endl;
+        }
 };
-
 class B:public A
 {
     public:
@@ -16,14 +19,18 @@ class B:public A
         {
             cout<<"Display function from class B"<<endl;
         }
+        void show()
+        {
+            cout<<"Show function from class B"<<endl;
+        }
 };
 int main()
 {
-  
-    A *P,A1;
-    P = &A1;
-    P->display();
     B B1;
+    B1.display();
+    B1.show();
+    A *P;
     P = &B1;
     P->display();
+    P->show();
 }
